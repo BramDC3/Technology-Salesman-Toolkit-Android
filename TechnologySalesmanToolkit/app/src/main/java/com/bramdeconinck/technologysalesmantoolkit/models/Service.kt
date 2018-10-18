b701fc9655412bbf8d3b8b3a0cbb09e515b74fe3
Bram De Coninck <Bram@bramdeconinck.com>
2018-10-18 13:30:36 +0200

    Added a category enum

--- TechnologySalesmanToolkit/app/src/main/java/com/bramdeconinck/technologysalesmantoolkit/models/Service.kt
+++ TechnologySalesmanToolkit/app/src/main/java/com/bramdeconinck/technologysalesmantoolkit/models/Service.kt
@@ -2,4 +2,4 @@ package com.bramdeconinck.technologysalesmantoolkit.models
 
 import com.google.firebase.Timestamp
 
-data class Service(val id: String, val name: String, val description: String, val category: Int, val created: Timestamp, val price: Double, val image: String, val url: String)
+data class Service(val id: String, val name: String, val description: String, val category: Category, val created: Timestamp, val price: Double, val image: String, val url: String)
--- TechnologySalesmanToolkit/app/src/main/java/com/bramdeconinck/technologysalesmantoolkit/network/FirestoreAPI.kt
+++ TechnologySalesmanToolkit/app/src/main/java/com/bramdeconinck/technologysalesmantoolkit/network/FirestoreAPI.kt
@@ -2,6 +2,7 @@ package com.bramdeconinck.technologysalesmantoolkit.network
 
 import android.content.ContentValues.TAG
 import android.util.Log
+import com.bramdeconinck.technologysalesmantoolkit.models.Category
 import com.bramdeconinck.technologysalesmantoolkit.models.Service
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.QueryDocumentSnapshot
@@ -34,12 +35,22 @@ class FirestoreAPI {
                 id = snapshot.id,
                 name = snapshot.getString("name")!!,
                 description = snapshot.getString("description")!!,
-                category = snapshot.getDouble("category")!!.toInt(),
+                category = fromIntToCategory(snapshot.getDouble("category")!!.toInt()),
                 created = snapshot.getTimestamp("created")!!,
                 price = snapshot.getDouble("price")!!,
                 image = snapshot.getString("image")!!,
                 url = snapshot.getString("url")!!
         )
+    }
+
+    //Retrieve the right category based on an integer
+    private fun fromIntToCategory(i: Int): Category {
+        return when (i) {
+            0 -> Category.Windows
+            1 -> Category.Android
+            2 -> Category.Apple
+            else -> Category.Other
+        }
     }
 
 }
