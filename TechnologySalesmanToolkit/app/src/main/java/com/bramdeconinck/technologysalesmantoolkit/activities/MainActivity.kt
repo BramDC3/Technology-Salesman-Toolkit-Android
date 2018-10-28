69becaeb28c17c602601ed6a8b700371069246a7
Bram De Coninck <Bram@bramdeconinck.com>
2018-10-28 23:25:52 +0100

    Final push before conditional navigation

--- TechnologySalesmanToolkit/app/src/main/java/com/bramdeconinck/technologysalesmantoolkit/activities/MainActivity.kt
+++ TechnologySalesmanToolkit/app/src/main/java/com/bramdeconinck/technologysalesmantoolkit/activities/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.bramdeconinck.technologysalesmantoolkit.activities
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import com.bramdeconinck.technologysalesmantoolkit.R
-import com.bramdeconinck.technologysalesmantoolkit.fragments.LoginFragment
-import com.bramdeconinck.technologysalesmantoolkit.fragments.ServiceListFragment
-import com.google.firebase.auth.FirebaseAuth
+
 
 class MainActivity : AppCompatActivity() {
 
