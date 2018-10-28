b9e38c80fc312915e86e86e49cee8f883dc0c957
Bram De Coninck <Bram@bramdeconinck.com>
2018-10-28 23:42:32 +0100

    Created conditional navigation

--- TechnologySalesmanToolkit/app/src/main/java/com/bramdeconinck/technologysalesmantoolkit/activities/MainActivity.kt
+++ TechnologySalesmanToolkit/app/src/main/java/com/bramdeconinck/technologysalesmantoolkit/activities/MainActivity.kt
@@ -2,14 +2,32 @@ package com.bramdeconinck.technologysalesmantoolkit.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import androidx.navigation.findNavController
 import com.bramdeconinck.technologysalesmantoolkit.R
+import com.bramdeconinck.technologysalesmantoolkit.R.id.nav_host_fragment
+import com.google.firebase.auth.FirebaseAuth
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        findNavController(nav_host_fragment).addOnNavigatedListener { controller, destination ->
+            when (destination.id) {
+                R.id.serviceListFragment -> {
+                    if (mAuth.currentUser == null) {
+                        findNavController(nav_host_fragment).popBackStack(R.id.serviceListFragment, true)
+                        findNavController(nav_host_fragment).navigate(R.id.loginFragment)
+                    }
+                }
+            }
+        }
     }
 
 }
