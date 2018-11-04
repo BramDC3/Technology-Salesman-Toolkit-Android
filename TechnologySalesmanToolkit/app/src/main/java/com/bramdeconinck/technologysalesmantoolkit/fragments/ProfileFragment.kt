39bec947528b9213d7c740b7e8c6b851e7d18dad
Bram De Coninck <bramdeconinck3@gmail.com>
2018-11-04 21:24:13 +0100

    Started working on the code for the profile fragment

--- TechnologySalesmanToolkit/app/src/main/java/com/bramdeconinck/technologysalesmantoolkit/fragments/ProfileFragment.kt
+++ TechnologySalesmanToolkit/app/src/main/java/com/bramdeconinck/technologysalesmantoolkit/fragments/ProfileFragment.kt
@@ -7,11 +7,28 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.bramdeconinck.technologysalesmantoolkit.R
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.fragment_profile.view.*
 
 class ProfileFragment : Fragment() {
 
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val rootView =  inflater.inflate(R.layout.fragment_profile, container, false)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        if (mAuth.currentUser != null) {
+            val firebaseUser: FirebaseUser = mAuth.currentUser!!
+            Glide.with(this).load(firebaseUser.photoUrl).into(rootView.img_profile_image)
+            rootView.txt_profile_name.setText(firebaseUser.displayName)
+            rootView.txt_profile_email.setText(firebaseUser.email)
+        }
+
+        return rootView
     }
 
 }
