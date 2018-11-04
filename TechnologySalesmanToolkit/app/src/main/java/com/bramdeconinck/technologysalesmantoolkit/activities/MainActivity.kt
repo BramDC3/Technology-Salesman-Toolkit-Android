15c5e01108dde6f4df7ced3ed7b2389eef7f8029
Bram De Coninck <bramdeconinck3@gmail.com>
2018-11-04 19:59:42 +0100

    Added a custom toolbar, bottom navigation bar and created functions for hiding and showing them

--- TechnologySalesmanToolkit/app/src/main/java/com/bramdeconinck/technologysalesmantoolkit/activities/MainActivity.kt
+++ TechnologySalesmanToolkit/app/src/main/java/com/bramdeconinck/technologysalesmantoolkit/activities/MainActivity.kt
@@ -2,11 +2,11 @@ package com.bramdeconinck.technologysalesmantoolkit.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
+import android.view.View
 import androidx.navigation.findNavController
 import com.bramdeconinck.technologysalesmantoolkit.R
-import com.bramdeconinck.technologysalesmantoolkit.R.id.nav_host_fragment
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,18 +20,51 @@ class MainActivity : AppCompatActivity() {
         // The Firebase Authentication instance
         mAuth = FirebaseAuth.getInstance()
 
+        // Using the custom toolbar as support action bar
+        setSupportActionBar(custom_toolbar)
+
         // The home of our nav graph is the ServiceListFragment, but
         // Users need to be logged in in order to do so
         // This method redirects users to the LoginFragment if they aren't logged in
-        findNavController(nav_host_fragment).addOnNavigatedListener { _, destination ->
+        findNavController(R.id.nav_host_fragment).addOnNavigatedListener { _, destination ->
             when (destination.id) {
                 R.id.loginFragment -> {
                     if (mAuth.currentUser != null) {
-                        findNavController(nav_host_fragment).popBackStack(R.id.loginFragment, true)
-                        findNavController(nav_host_fragment).navigate(R.id.serviceListFragment)
+                        findNavController(R.id.nav_host_fragment).popBackStack(R.id.loginFragment, true)
+                        findNavController(R.id.nav_host_fragment).navigate(R.id.serviceListFragment)
+                    } else {
+                        hideToolbarAndBottomNavigation()
                     }
                 }
+                R.id.registrationFragment -> hideToolbarAndBottomNavigation()
+                else -> showToolbarAndBottomNavigation()
             }
+        }
+    }
+
+    // We don't want to show the toolbar and bottom navigation
+    // On the login and registration screen, so we hide them
+    private fun hideToolbarAndBottomNavigation() {
+        supportActionBar?.hide()
+        with(bottom_navigation_view) {
+            if (visibility == View.VISIBLE && alpha == 1f) {
+                animate()
+                        .alpha(0f)
+                        .withEndAction { visibility = View.GONE }
+                        .duration = 0
+            }
+        }
+    }
+
+    // We do want to show the toolbar and bottom navigation
+    // On the other screens, so we make them visible again
+    private fun showToolbarAndBottomNavigation() {
+        supportActionBar?.show()
+        with(bottom_navigation_view) {
+            visibility = View.VISIBLE
+            animate()
+                    .alpha(1f)
+                    .duration = 0
         }
     }
 
--- TechnologySalesmanToolkit/app/src/main/java/com/bramdeconinck/technologysalesmantoolkit/fragments/ServiceListFragment.kt
+++ TechnologySalesmanToolkit/app/src/main/java/com/bramdeconinck/technologysalesmantoolkit/fragments/ServiceListFragment.kt
@@ -7,7 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.bramdeconinck.technologysalesmantoolkit.R
-import com.bramdeconinck.technologysalesmantoolkit.activities.MainActivity
 import com.bramdeconinck.technologysalesmantoolkit.adapters.ServiceAdapter
 import com.bramdeconinck.technologysalesmantoolkit.interfaces.IFirebaseCallback
 import com.bramdeconinck.technologysalesmantoolkit.models.Service
