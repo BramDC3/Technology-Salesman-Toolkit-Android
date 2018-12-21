18d96358a56824459ffdc944e6ba879969fae02f
Bram De Coninck <bram@bramdeconinck.com>
2018-12-21 23:51:31 +0100

    Fixed a bug caused by the Category enum

--- TechnologySalesmanToolkit/app/src/main/java/com/bramdeconinck/technologysalesmantoolkit/utils/RoomConvertersUtils.kt
+++ TechnologySalesmanToolkit/app/src/main/java/com/bramdeconinck/technologysalesmantoolkit/utils/RoomConvertersUtils.kt
@@ -1,6 +1,7 @@
 package com.bramdeconinck.technologysalesmantoolkit.utils
 
 import android.arch.persistence.room.TypeConverter
+import com.bramdeconinck.technologysalesmantoolkit.models.Category
 import com.google.firebase.Timestamp
 import java.util.*
 
@@ -16,6 +17,28 @@ object RoomConvertersUtils {
     @JvmStatic
     fun toTimestamp(timestamp: Timestamp?): Long? {
         return timestamp?.toDate()?.time
+    }
+
+    @TypeConverter
+    @JvmStatic
+    fun fromCategory(value: Int?): Category? {
+        return when (value) {
+            0 -> Category.Windows
+            1 -> Category.Android
+            2 -> Category.Apple
+            else -> Category.Andere
+        }
+    }
+
+    @TypeConverter
+    @JvmStatic
+    fun toCategory(category: Category): Int? {
+        return when (category) {
+            Category.Windows -> 0
+            Category.Android -> 1
+            Category.Apple -> 2
+            Category.Andere -> 3
+        }
     }
 
 }
